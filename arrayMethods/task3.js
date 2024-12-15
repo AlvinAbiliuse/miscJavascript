c24573cf816018176f22702109be33ddbe1954c5
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-16 01:35:53 +0400

    task3.js actually completed

--- arrayMethods/task3.js
+++ arrayMethods/task3.js
@@ -1,8 +1,9 @@
 
 function filterRange(arr, n1, n2) {
-	let newArr= arr.filter((n) => n < n1 || n > n2);
-	return newArr;
+	array= arr.filter((n) => n < n1 || n > n2);
+	
 }
 
 let array = [1, 2, 5, 10, 16, 20, 6, 3];
-console.log(filterRange(array, 1, 4));
+filterRange(array, 1, 4);
+console.log(array);
