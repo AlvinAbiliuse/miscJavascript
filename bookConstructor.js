61602e58d8171f68b8f3455f9234c666794e767d
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-29 09:09:31 +0400

    bookConstructor.js

---
+++ bookConstructor.js
@@ -0,0 +1,15 @@
+
+
+function book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+
+function info() {
+
+
+}
+
+const bookInfo = new book(
