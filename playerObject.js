b561bed7751a2962cefa73b8951f6a70be1b7b82
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-30 08:45:47 +0400

    playerObject.js

--- playerObject.js
+++ playerObject.js
@@ -1,21 +1,31 @@
 
 const log = console.log;
 
+function Person(name) {
+	this.name = name;
+}
+
+Person.prototype.sayName = function() {
+	log(`Hello, I'm ${this.name}`);
+}
+
+
 function Player(name, marker) {
 	this.name = name;
 	this.marker = marker;
-	this.sayName = function() {
-		log(this.name)
-	}
 }
-Player.prototype.sayHello = function() {
-	log("Hello, I'm a player!");
+Player.prototype.getMarker = function() {
+	log(`My marker is '${this.marker}'`);
 }
+
+Object.setPrototypeOf(Player.prototype, Person.prototype);
+Object.getPrototypeOf(Player.prototype);
 
 const player1 = new Player("steve", "X");
 const player2 = new Player("also steve", "O");
+
 player1.sayName();
 player2.sayName();
 
-player1.sayHello();
-player2.sayHello();
+player1.getMarker();
+player2.getMarker();
