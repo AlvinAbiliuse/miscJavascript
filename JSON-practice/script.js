703dcbfa65ecbdd03faf9956c3a074d0db5c77fe
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-16 05:09:01 +0400

    progress

--- JSON-practice/script.js
+++ JSON-practice/script.js
@@ -1,15 +1,28 @@
 
 
 async function populate() {
+	console.log("working");
 	const requestURL = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
 
 	const request = new Request(requestURL);
 	
 	const response = await fetch(request);
+	console.log(response);
 	const superHeroes = await response.json();
+	console.log(superHeroes);
 
 	populateHeader(superHeroes);
 	populateHeroes(superHeroes);
-}
+};
+
+function populateHeader(heroes) {
+	let x = 1;	
+};
+
+function populateHeroes(heroes) {
+	let x = 1;	
+
+};
 
 
+populate();
