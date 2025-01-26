09e925e7476256d099153f2d31f9ed8bcbae0211
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-27 02:25:27 +0400

    script.js

---
+++ formValidationJS/odinFormValidation/script.js
@@ -0,0 +1,10 @@
+let email = document.getElementById("email");
+let country = document.getElementById("country");
+let zipcode = document.getElementById("zipcode");
+let pass = document.getElementById("pass");
+let passConfirm = document.getElementById("passConfirm");
+let form = document.querySelector("form");
+
+function checkValidity() {
+	let af = af;
+}
