34dd343f413cb1779897de80f583d625ed9f9231
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-27 06:08:36 +0400

    progress

--- formValidationJS/odinFormValidation/script.js
+++ formValidationJS/odinFormValidation/script.js
@@ -1,25 +1,55 @@
-let email = document.getElementById("email");
-let country = document.getElementById("country");
-let zipcode = document.getElementById("zipcode");
-let pass = document.getElementById("pass");
-let passConfirm = document.getElementById("passConfirm");
-let form = document.querySelector("form");
+let email = [document.getElementById("email"), "Please enter a valid email!"];
+let country = [
+	document.getElementById("country"),
+	"Please enter a valid country!",
+];
+let zipcode = [
+	document.getElementById("zipcode"),
+	"Please enter a valid Zip Code!",
+];
+let pass = [document.getElementById("pass"), "Please enter a password"];
+let passConfirm = [
+	document.getElementById("passConfirm"),
+	"password confirmation does not match the password!",
+];
+let form = [document.querySelector("form")];
 
-function checkValidity() {
-	if (!email.checkValidity.validiti) {
-		console.log(email.value);
-		email.nextElementSibling.textContent = "yolo";
+function checkValidity(data) {
+	if (!data[0].validity.valid) {
+		data[0].nextElementSibling.textContent = data[1];
+		data[0].classList.add("invalid");
+	} else {
+		data[0].classList.remove("invalid");
+		data[0].nextElementSibling.textContent = "";
 	}
 }
 
-function checkPass() {
-	if (pass.value != passConfirm.value) {
+function passCheck(e) {
+	console.log(pass.value);
+	if (pass.value.length == 0) {
+		pass[0].nextElementSibling.textContent = pass[1];
+		e.preventDefault();
+	} else {
+	}
+	pass[0].nextElementSibling.textContent = "";
+	if ((pass.value.length = pass.value != passConfirm.value)) {
+		console.log("pass");
 	}
 }
 
-email.addEventListener("focusout", checkValidity);
-country.addEventListener("focusout", checkValidity);
-zipcode.addEventListener("focusout", checkValidity);
-pass.addEventListener("focusout", checkValidity);
-passConfirm.addEventListener("focusout", checkValidity);
-form.addEventListener("submit", checkValidity);
+function formCheck() {}
+
+email[0].addEventListener("focusout", () => {
+	checkValidity(email);
+});
+country[0].addEventListener("focusout", () => {
+	checkValidity(country);
+});
+zipcode[0].addEventListener("focusout", () => {
+	checkValidity(zipcode);
+});
+passConfirm[0].addEventListener("focusout", passCheck);
+form[0].addEventListener("submit", (e) => {
+	passCheck(e);
+	e.preventDefault();
+});
