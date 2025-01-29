31a795a1a92cb09a2a25a1acb98572a9ae379e43
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-30 01:59:01 +0400

    added eventlistener to button to get new image

--- giphyAPI/script.js
+++ giphyAPI/script.js
@@ -1,11 +1,12 @@
+let newImage = document.querySelector(".newImg");
+let s = "cats";
+let key = "sr3YmfucDXY2tWhybfwINnvqbsDSqSRw";
+
 function getImage(search) {
 	const img = document.querySelector("img");
-	fetch(
-		`https://api.giphy.com/v1/gifs/translate?api_key=sr3YmfucDXY2tWhybfwINnvqbsDSqSRw&s=${search}`,
-		{
-			mode: "cors",
-		}
-	)
+	fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${search}`, {
+		mode: "cors",
+	})
 		.then(function (response) {
 			return response.json();
 		})
@@ -14,4 +15,5 @@ function getImage(search) {
 		});
 }
 
-getImage("cats");
+getImage(s);
+newImage.addEventListener("click", () => getImage(s));
