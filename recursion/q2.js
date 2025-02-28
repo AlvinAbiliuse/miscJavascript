1c7492f6e983ed832daa68b2b5c80d66df3368d3
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-28 05:41:07 +0400

    q2.js

--- recursion/q2.js
+++ recursion/q2.js
@@ -1,6 +1,6 @@
-function power(n1, n2) {
-	if (n2 === 0) return 1;
-	return n1 * power(n1, n2 - 1);
+function power(base, exponent) {
+	if (exponent === 0) return 1;
+	return base * power(base, exponent - 1);
 }
 
 console.log(power(2, 4)); // 16
