6a7e898d213a16df7c3e3c468da89eeba7930610
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-01 02:02:08 +0400

    q9.js

---
+++ recursion/q9.js
@@ -0,0 +1,8 @@
+function replicate(count, num) {
+	if (count <= 0) return [];
+	return [num] + " " + replicate(count - 1, num);
+}
+
+console.log(replicate(3, 5)); // [5, 5, 5]
+console.log(replicate(1, 69)); // [69]
+console.log(replicate(-2, 6)); // []
