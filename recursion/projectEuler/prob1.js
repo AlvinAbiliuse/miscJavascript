627c0216bfef21684d551d79ad73d0d5cbb30ffe
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-10 06:47:28 +0400

    prob1.js done

--- recursion/projectEuler/prob1.js
+++ recursion/projectEuler/prob1.js
@@ -1,11 +1,12 @@
 function multiple(max, current = 0) {
 	if (max < 3) return 0;
-	if (current === max) return;
+	if (current === max) return 0;
 
-	let curr = 1;
-
-	if (current === 3 || current === 5) curr = current;
-	return curr * multiple(max, current++);
+	let curr = 0;
+	if (current % 3 === 0 || current % 5 === 0) {
+		curr = current;
+	}
+	return curr + multiple(max, current + 1);
 }
 
-console.log(multiple(10));
+console.log(multiple(1000));
