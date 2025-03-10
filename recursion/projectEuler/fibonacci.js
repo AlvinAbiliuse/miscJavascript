0335e065a554499b8acfc80fc4803649ff9538c1
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-10 06:59:15 +0400

    fibonacci.js

--- recursion/projectEuler/fibonacci.js
+++ recursion/projectEuler/fibonacci.js
@@ -1,44 +1,10 @@
-function fib(n) {
-	let count = n;
-	let n1 = 0;
-	let n2 = 1;
-	let arr = [];
-	while (count !== 0) {
-		count--;
-		arr.push(n1);
-		let current = n1;
-		n1 = n2;
-		n2 = current + n2;
+function fibRecEven(c, n1 = 0, n2 = 1) {
+	if (c == 0) return 0;
+	if (n1 % 2) {
+		return fibRecEven(c - 1, n2, n1 + n2);
+	} else {
+		return n1 + fibRecEven(c - 1, n2, n1 + n2);
 	}
-	return arr;
-	/* comma seperated string version:
-	return arr.join(", ")
-	*/
 }
 
-function fibRec(c, n1 = 0, n2 = 1) {
-	if (c == 0) return "";
-	return [n1, ...fibRec(c - 1, n2, n1 + n2)];
-	/* comma seperate string version: 
-	return c == 1 ? n1 : `${n1}, ` + fibRec(c - 1, n2, n2 + n1);
-	*/
-}
-
-console.log("\nFibonacci\n");
-console.log("\nIterative Version:\n");
-let iterativeTime = new Date().getTime();
-console.log(fib(20));
-console.log(
-	"\nElapsed Time (seconds): ",
-	(new Date().getTime() - iterativeTime) / 1000
-);
-console.log("\n\nRecursive Version:\n");
-let recursiveTime = new Date().getTime();
-console.log(fibRec(20));
-console.log(
-	"\nElapsed Time (seconds): ",
-	(new Date().getTime() - recursiveTime) / 1000
-);
-console.log("\n");
-
-export { fib, fibRec };
+console.log(fibRecEven(10));
